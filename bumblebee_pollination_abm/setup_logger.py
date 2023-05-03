c0fe573a072da7686bcab37d3cfef0a0b6241978
chiara-camilla-rambaldi-migliore <cami.rambaldi@hotmail.it>
2023-05-03 10:35:20 +0200

    fix log directory creation

--- bumblebee_pollination_abm/setup_logger.py
+++ bumblebee_pollination_abm/setup_logger.py
@@ -1,9 +1,13 @@
 import logging
 import time
+import os
 
 
 datetime = time.strftime("%Y%m%d-%H%M%S")
-logging.basicConfig(filename=f"logs/urban_{datetime}.log",
+filename = f"logs/urban_{datetime}.log"
+os.makedirs(os.path.dirname(filename), exist_ok=True)
+
+logging.basicConfig(filename=filename,
             filemode='a',
             format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
             datefmt='%H:%M:%S',
